f7d82d491987df03626cceae86fad8ffbacbc7bf
crims1n <pyromoosenac@gmail.com>
2022-09-03 23:26:01 -0400

    can't forget about mobile

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -3,9 +3,9 @@
     export let desc: string;
 </script>
 
-<div tabindex="0" class="card w-80 bg-nord2 shadow-xl text-nord4 dropdown-content">
-    <div class="card-body">
-        <h2 class="card-title">{title}</h2>
-        <p>{desc}</p>
+<div tabindex="0" class="card xl:w-80 bg-nord2 shadow-xl text-nord4 dropdown-content">
+    <div class="card-body p-2 xl:p-6">
+        <h2 class="text-xs card-title xl:text-2xl">{title}</h2>
+        <p class="text-xs xl:text-base">{desc}</p>
     </div>
 </div>
