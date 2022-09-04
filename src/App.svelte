5999038543128b37cf989d873b2366b75ac5e1c1
crims1n <pyromoosenac@gmail.com>
2022-09-03 21:40:25 -0400

    fixed scaling issues

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,6 @@
 
 <main class="h-screen">
     <Header />
-    <div class="h-full w-full bg-nord0 pt-20 px-0 pb-0 md:pt-32 xl:px-20 xl:pb-20"><Schedule /></div>
+    <div class=" h-4/6 w-full sm:h-full bg-nord0 pt-20 px-0 pb-12 md:pt-32 xl:px-20 xl:pb-20"><Schedule /></div>
     <Footer />
 </main>
